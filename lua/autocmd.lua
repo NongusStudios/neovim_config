4457813afa48cafea84ca4b5f4d6fa72bdbd02ad
NongusStudios <jandrewgadd@gmail.com>
2024-06-08 22:55:34 +1200

    Updated to lua configuration

---
+++ lua/autocmd.lua
@@ -0,0 +1,31 @@
+local A = vim.api
+
+-- Custom filetypes
+vim.filetype.add({
+    extension = {
+        conf = 'conf',
+        mdx = 'markdown',
+        mjml = 'html',
+        kicad_mod = 'scheme',
+    },
+    pattern = {
+        ['.*%.env.*'] = 'sh',
+        ['ignore$'] = 'conf',
+    },
+    filename = {
+        ['yup.lock'] = 'yaml',
+    },
+})
+
+-- Lint AU
+A.nvim_create_autocmd({ "BufWritePost" }, {
+  callback = function()
+    -- try_lint without arguments runs the linters defined in `linters_by_ft`
+    -- for the current filetype
+    require("lint").try_lint()
+  end,
+})
+
+-- Float Term
+vim.cmd('autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()')
+
---
+++ lua/keybinds.lua
@@ -0,0 +1,20 @@
+local function map(m, k, v)
+    vim.keymap.set(m, k, v, { silent = true })
+end
+
+-- NvimTree 
+map("n", "<leader>t", "<CMD>NvimTreeToggle<CR>")
+
+-- Floating terminal
+map("n", "<leader>f", "<CMD>ToggleTerm direction=float<CR>")
+
+function _G.set_terminal_keymaps()
+    local opts = {buffer = 0}
+    vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+    vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+    vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+    vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+    vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+    vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+    vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+end
