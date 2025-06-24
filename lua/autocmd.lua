25afbf9c9bd07d47eed1bf3dd089ffd5db194549
Joel Gadd <jandrewgadd@proton.me>
2025-06-25 01:15:33 +1200

    Removed auto-complete and linting plugins to pivot to a simpler general text editor config

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -17,15 +17,6 @@ vim.filetype.add({
     },
 })
 
--- Lint AU
-A.nvim_create_autocmd({ "BufWritePost" }, {
-  callback = function()
-    -- try_lint without arguments runs the linters defined in `linters_by_ft`
-    -- for the current filetype
-    require("lint").try_lint()
-  end,
-})
-
 -- Float Term
 vim.cmd('autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()')
 
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -27,20 +27,6 @@ local plugins = {
         -- use opts = {} for passing setup options
         -- this is equalent to setup({}) function
     },
-    { -- Tree Sitter
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        config = function () 
-          local configs = require("nvim-treesitter.configs")
-
-          configs.setup({
-              ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
-              sync_install = false,
-              highlight = { enable = true },
-              indent = { enable = true },  
-            })
-        end
-    },
     { -- nvim tree
         "nvim-tree/nvim-tree.lua",
         version = "*",
@@ -51,33 +37,6 @@ local plugins = {
         config = function()
             require("nvim-tree").setup {}
         end,
-    },
-    { -- Mason
-        "williamboman/mason.nvim",
-    },
-    { -- Formatter
-        "mhartington/formatter.nvim",
-    },
-    { -- LSP Config
-        "williamboman/mason-lspconfig.nvim",
-        "neovim/nvim-lspconfig",
-    },
-    { -- Dap
-        "rcarriga/nvim-dap-ui", dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"}
-    },
-    { -- Lint
-        "mfussenegger/nvim-lint"
-    },
-
-    { -- Autocomplete
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-        "hrsh7th/nvim-cmp",
-    },
-    { -- Neodev
-        "folke/neodev.nvim", opts = {}
     },
     { -- Toggle Term
         'akinsho/toggleterm.nvim', version = "*", config = true
@@ -91,67 +50,12 @@ local plugins = {
 
 -- Setup plugins
 require("lazy").setup(plugins)
-require("mason").setup()
-
-local cmp = require("cmp")
-cmp.setup {
-    snippet = {
-      -- REQUIRED - you must specify a snippet engine
-      expand = function(args)
-        -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-        -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-        vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
-      end,
-    },
-    window = {
-      -- completion = cmp.config.window.bordered(),
-      -- documentation = cmp.config.window.bordered(),
-    },
-    mapping = cmp.mapping.preset.insert({
-      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      ['<C-Space>'] = cmp.mapping.complete(),
-      ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    }),
-    sources = cmp.config.sources({
-      { name = 'nvim_lsp' },
-      -- { name = 'vsnip' }, -- For vsnip users.
-      -- { name = 'luasnip' }, -- For luasnip users.
-      -- { name = 'ultisnips' }, -- For ultisnips users.
-      -- { name = 'snippy' }, -- For snippy users.
-    }, {
-      { name = 'buffer' },
-    })
-}
 
 -- Set up lspconfig.
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-require("mason-lspconfig").setup {
-    ensure_installed = { "lua_ls", "rust_analyzer", "clangd", "zls", "taplo" }
-}
-require("lspconfig").lua_ls.setup {capabilities = capabilities}
-require("lspconfig").rust_analyzer.setup {capabilities = capabilities}
-require("lspconfig").clangd.setup {capabilities = capabilities}
-require("lspconfig").zls.setup {capabilities = capabilities}
-require("lspconfig").taplo.setup {capabilities = capabilities}
-
+--local capabilities = require('cmp_nvim_lsp').default_capabilities()
 require('lualine').setup {
     config = { theme = "gruvbox" },
     ...
 }
 
 require("nvim-tree").setup()
-
-require('lint').linters_by_ft = {
-  markdown = {'vale',}
-}
-
-require("neodev").setup({
-  library = { plugins = { "nvim-dap-ui" }, types = true },
-  ...
-})
-
-
